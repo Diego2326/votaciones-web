d286534818f27d972980c0b99eb54918e6b93476
diego <diegoe3b@gmail.com>
2026-04-08 11:28:25 -0600

    refactor: restructure application setup and remove unused code

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import '@/core/utils/runtime'
+import '@/styles/index.css'
+import { AppProviders } from '@/app/providers'
+import { AppRouter } from '@/app/router'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AppProviders>
+      <AppRouter />
+    </AppProviders>
   </StrictMode>,
 )
